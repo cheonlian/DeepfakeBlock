6b7a49bfd8c853eb55b17badd10a552990d30175
Jinpyo Hong <hojp7874@gmail.com>
2021-03-09 21:22:29 +0900

    develop branch create

--- api/api/views.py
+++ api/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from django.http import FileResponse
+from django.http import FileResponse, response
 from .serializers import ImageSerializer
 
 from django.views import View
@@ -11,26 +11,53 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+from PIL import Image as pilImage
+
 @api_view(['GET'])
 def index(request, id):
-    # img = open('media/iu_ori.png', 'rb')
-    # response = FileResponse(img)
-    print('###1')
     image = Image.objects.get(id=id)
-    print('###2')
-    print(image)
     serializer = ImageSerializer(image)
-    print('###3')
-    print(serializer)
-    print(serializer.data)
     return Response(serializer.data)
 
+
+
+from tog.dataset_utils.preprocessing import letterbox_image_padded
+from keras import backend as K
+from tog.models.yolov3 import YOLOv3_Darknet53_Face
+from tog.tog.attacks import *
+import tensorflow as tf
+
+K.clear_session()
+global graph
+graph = tf.get_default_graph()
+
+tf_config = tf.ConfigProto()
+tf_config.gpu_options.allow_growth = True
+weights = 'tog/model_weights/yolo_face.h5'
+detector = YOLOv3_Darknet53_Face(weights=weights)
+
+def predict(input):
+    eps = 8 / 255.       
+    eps_iter = 2 / 255.  
+    n_iter = 10        
+    x_query, x_meta = letterbox_image_padded(input, size=detector.model_img_size)
+    with graph.as_default():
+        x_adv_untargeted = tog_untargeted(victim=detector, x_query=x_query, n_iter=n_iter, eps=eps, eps_iter=eps_iter)
+    img = x_adv_untargeted[0]*255
+    output = pilImage.fromarray(img.astype('uint8'), 'RGB')
+    return output
 
 @api_view(['POST'])
 def post(request):
     input_image = request.data['input_image']
-    print(type(input_image))
     serializer = ImageSerializer(data=request.data)
+    input_img = pilImage.open(input_image)
+
+    output = predict(input_img)
+    output.save("media/adv.png")
+    response = FileResponse(open("media/adv.png", "rb"))
+    return response
+
     if serializer.is_valid(raise_exception=True):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
---
+++ api/tog/dataset_utils/preprocessing.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import numpy as np
+
+
+def letterbox_image_padded(image, size=(416, 416)):
+    """ Resize image with unchanged aspect ratio using padding """
+    image_copy = image.copy()
+    iw, ih = image_copy.size
+    w, h = size
+    scale = min(w / iw, h / ih)
+    nw = int(iw * scale)
+    nh = int(ih * scale)
+
+    image_copy = image_copy.resize((nw, nh), Image.BICUBIC)
+    new_image = Image.new('RGB', size, (0, 0, 0))
+    new_image.paste(image_copy, ((w - nw) // 2, (h - nh) // 2))
+    new_image = np.asarray(new_image)[np.newaxis, :, :, :] / 255.
+    meta = ((w - nw) // 2, (h - nh) // 2, nw + (w - nw) // 2, nh + (h - nh) // 2, scale)
+
+    return new_image, meta
+
+def no_padded(image):
+    image = np.asarray(image) / 255.
+    if image.shape[2] == 4:
+        image = np.delete(image, 3, axis=2)
+    image = image[np.newaxis, :, :, :]
+    return image
