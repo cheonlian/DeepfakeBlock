4be14a44f9252a0976b1add5b5552ef2ae3121a1
Henry Lee <hugh.q.lee@gmail.com>
2021-03-09 19:22:08 +0900

    front-web 1.0

--- api/api/views.py
+++ api/api/views.py
@@ -11,27 +11,27 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def index(request, id):
     # img = open('media/iu_ori.png', 'rb')
     # response = FileResponse(img)
-    print('###1')
+    print("###1")
     image = Image.objects.get(id=id)
-    print('###2')
+    print("###2")
     print(image)
     serializer = ImageSerializer(image)
-    print('###3')
+    print("###3")
     print(serializer)
     print(serializer.data)
     return Response(serializer.data)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def post(request):
-    input_image = request.data['input_image']
+    input_image = request.data["input_image"]
     print(type(input_image))
     serializer = ImageSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
